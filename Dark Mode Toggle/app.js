7649194ec0041be2e1c5f74041f7afccb6b02297
surjit2001 <kausiksurjit@gmail.com>
2021-08-23 01:21:27 +0530

    commit

---
+++ Dark Mode Toggle/app.js
@@ -0,0 +1,16 @@
+const checkBox=document.querySelector('input[name="theme');
+const htmlElement=document.documentElement;
+// console.log(htmlElement);
+
+checkBox.addEventListener('click',() =>{
+    if(checkBox.checked)
+    {
+        htmlElement.classList.toggle('transition');
+        htmlElement.setAttribute('data-theme','dark');
+    }
+    else
+    {
+        htmlElement.classList.toggle('transition');
+        htmlElement.setAttribute('data-theme','light');
+    }
+});
